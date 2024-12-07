e23afc96c4451f8fcfbf5bd930cd8558f604563f
Segun Ade <giftsegunade@gmail.com>
2024-12-07 14:27:10 +0100

    text update

--- cmd/ExpenseSummary.go
+++ cmd/ExpenseSummary.go
@@ -8,7 +8,7 @@ import (
 var ExpenseSummaryCmd = &cobra.Command{
 	Use:     "Expense Summary",
 	Aliases: []string{"summary"},
-	Short:   "Gives am expense  summary",
+	Short:   "Gives an expense  summary",
 	Long: `summary for expenses, can give summary for a particular month with the --month flag eg:  expense-tracker summary --month 8 with a response like: # Total expenses for August: $20
 `,
 	Run: func(cmd *cobra.Command, args []string) {
